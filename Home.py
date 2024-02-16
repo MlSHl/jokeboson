0987ba634bca2c3bb13655eb6869ff12e7d86a24
mishi <mishi.beraia@gmail.com>
2024-02-17 00:25:15 +0400

    Updated university

--- Home.py
+++ Home.py
@@ -5,7 +5,7 @@ st.set_page_config(page_title="Quantum Weight Loss Solution", page_icon="🌌")
 
 # Main title and author information
 st.title("Unveiling the Quantum Weight Loss Solution: Ditching Higgs Boson Interaction")
-st.markdown("*By Cosmos Quantumson, Theoretical Degree in Physics at South Harmon Institute of Technology (SHIT)*")
+st.markdown("*By Cosmos Quantumson, Theoretical Degree in Physics at South Harmon Institute of Technology *")
 
 # Main content
 st.image("Designer.png", use_column_width=True)
